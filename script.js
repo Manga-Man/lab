634805bf716359cff8434450ef9eb84e06677a80
Manga man <BetsegawG7@gmail.com>
2022-10-20 20:57:27 +0000

    stuff

---
+++ script.js
@@ -0,0 +1,14 @@
+let email = document.getElementsByClassName('email').value
+
+function start() {
+    var windowFeatures = "left=350,top=30,width=500,height=620";
+    window.open('./Auth259432856478563495842679348.html', '_blank', windowFeatures)
+}
+
+console.log(email)
+/*
+function auth() {
+    var windowFeatures = "left=100,top=100,width=520,height=620";
+    window.open('./Authdvskfvfc-vnsmvbsjvskfvdjskvbsd', '_blank', windowFeatures)
+}
+*/
